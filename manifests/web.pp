bc1d6cded3b5b0cdc1d84692d17a3f61aeda46af
Leandro Gonçalves <leandro.goncalves@hotelurbano.com.br>
2015-06-15 16:00:08 -0300

    primeiro commit

---
+++ manifests/web.pp
@@ -0,0 +1,47 @@
+exec { "apt-update" : 
+	command => "/usr/bin/apt-get update"
+}
+
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"] : 
+	ensure => installed,
+	require => Exec["apt-update"]
+}
+
+service { "tomcat7":
+	ensure => running,
+	enable => true,
+	hasstatus => true,
+	hasrestart => true,
+	require => Package["tomcat7"]
+}
+
+file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
+    source => "/vagrant/manifests/vraptor-musicjungle.war",
+    owner => "tomcat7",
+    group => "tomcat7",
+    mode => 0644,
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
+
+service { "mysql":
+	ensure => running,
+	enable => true,
+	hasstatus => true,
+	hasrestart => true,
+	require => Package["mysql-server"]
+}
+
+exec { "create database musicjungle" :
+	command => "mysqladmin -u root create musicjungle",
+	unless => "mysql -u root musicjungle",
+	path => "/usr/bin",
+	require => Service["mysql"]
+}
+
+exec { "create database user" :
+	command => "mysql -uroot -e \"GRANT ALL PRIVILEGES ON * TO 'musicjungle'@'%' IDENTIFIED BY 'minha-senha';\" musicjungle",
+    unless  => "mysql -umusicjungle -pminha-senha musicjungle",
+	path => "/usr/bin",
+	require => Service["mysql"]
+}
